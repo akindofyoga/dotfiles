63d8021455cedc2742c56212f09ba027d5a8143b
Roger Iyengar <ri@rogeriyengar.com>
2018-11-27 18:07:55 -0500

    Update init.el
    Added hook to delete trailing white space in Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -80,3 +80,5 @@
   kept-new-versions 20   ; how many of the newest versions to keep
   kept-old-versions 5    ; and how many of the old
   )
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
