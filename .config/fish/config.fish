b4b755fe35842454cb42de6c60b55e53a6f23700
Roger Iyengar <ri@rogeriyengar.com>
2019-08-28 18:10:07 -0400

    Moved environment variables from config.fish to .profile

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-set -gx PATH /usr/local/cuda-10.0/bin $PATH
-
-# Separating LD_LIBRARY_PATH with spaces causes an error in Tensorflow
-# This was the best way I could come up with to set this in a way that would work
-set -l old_ld_library_path ""
-if set -q LD_LIBRARY_PATH
-  set old_ld_library_path ":$LD_LIBRARY_PATH"
-end
-set -gx LD_LIBRARY_PATH "/usr/local/cuda-10.0/lib64:/usr/local/TensorRT-5.0.2.6/lib$old_ld_library_path"
